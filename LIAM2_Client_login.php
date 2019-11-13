9f585914d67aebc318f88e80d947d7a68def3f54
dwu300 <info@wuda.at>
2019-11-13 15:18:34 +0100

    vers after workshop

--- LIAM2_Client_login.php
+++ LIAM2_Client_login.php
@@ -11,46 +11,14 @@ if (isset($_POST['liam2_login'])) {
         $error = 'Please fill all the fields.';
     } else {
         $email_input = htmlspecialchars($_POST['email']);
-        $password_input = $_POST['password'];
-        $sentCode = htmlspecialchars($_POST['code']);
-        $captcha_result = (int)$_POST['result'];
-        if (getExpressionResult($sentCode) !== $captcha_result) {
-            $error = 'Wrong Captcha.';
-            $login_attempt_info = 'Not Successful - ' . $email_input . ' - ' . $error;
-            $result = api(json_encode(array(
-                    "cmd" => "create",
-                    "param" => array(
-                        "table" => "liam3_LoginAttempts",
-                        "row" => array(
-                            "liam3_LoginAttempts_time" => date('Y-m-d H:i'),
-                            "liam3_LoginAttempts_info" => $login_attempt_info
-                        )
-                    )
-                )
-            ));
+        if (!filter_var($email_input, FILTER_VALIDATE_EMAIL)) {
+            $error = "Email address '$email_input' is not valid.\n";
         } else {
-            $error = false;
-            $email = json_decode(api(json_encode(array("cmd" => "read", "param" => array("table" => "liam3_email",
-                "filter" => '{"=":["liam3_email_text","'.$email_input.'"]}')))), true);
-                $email = $email['records'];
-            if (!$email) {
-                $error = 'E-mail address is not recognized by the system, please enter the correct e-mail address or register.';
-                $login_attempt_info = 'Not Successful - ' . $email_input . ' - ' . $error;
-                $result = api(json_encode(array(
-                        "cmd" => "create",
-                        "paramparam" => array(
-                            "table" => "liam3_LoginAttempts",
-                            "row" => array(
-                                "liam3_LoginAttempts_time" => date('Y-m-d H:i'),
-                                "liam3_LoginAttempts_info" => $login_attempt_info
-                            )
-                        )
-                    )
-                ));
-            } elseif ($email[0]['state_id'] == EMAIL_STATE_VERIFIED) {
-                $email_id = $email[0]['liam3_email_id'];
-            } else {
-                $error = 'Email is not verified';
+            $password_input = $_POST['password'];
+            $sentCode = htmlspecialchars($_POST['code']);
+            $captcha_result = (int)$_POST['result'];
+            if (getExpressionResult($sentCode) !== $captcha_result) {
+                $error = 'Wrong Captcha.';
                 $login_attempt_info = 'Not Successful - ' . $email_input . ' - ' . $error;
                 $result = api(json_encode(array(
                         "cmd" => "create",
@@ -63,116 +31,169 @@ if (isset($_POST['liam2_login'])) {
                         )
                     )
                 ));
-            }
-            if (!$error) {
-                $user_email = json_decode(api(json_encode(array("cmd" => "read", "param" => array("table" => "liam3_user_email",
-                    "filter" => '{"=":["liam3_email_id",'.$email_id.']}')))), true);
-                    $user_email = $user_email['records'];
-                if ($user_email && ($user_email[0]['state_id'] == USER_EMAIL_STATE_USE)) {
-                    $user_id = $user_email[0]['liam3_User_id_fk_164887']['liam3_User_id'];
-                } else {
-                    $error = 'This email is unselected.';
-                    $login_attempt_info = 'Not Successful - ' . $email_input . ' - ' . $error;
-                    $result = api(json_encode(array(
-                            "cmd" => "create",
-                            "param" => array(
-                                "table" => "liam3_LoginAttempts",
-                                "row" => array(
-                                    "liam3_LoginAttempts_time" => date('Y-m-d H:i'),
-                                    "liam3_LoginAttempts_info" => $login_attempt_info
-                                )
-                            )
-                        )
-                    ));
-                }
-            }
-            if (!$error) {
-                $user = json_decode(api(json_encode(array("cmd" => "read", "param" => array("table" => "liam3_user",
-                    "filter" => '{"=":["liam3_User_id",'.$user_id.']}')))), true);
-                $user = $user['records'];
-                if (!$user) {
-                    $error = 'This email is not linked to any user.';
-                    $login_attempt_info = 'Not Successful - ' . $email_input . ' - ' . $error;
-                    $result = api(json_encode(array(
-                            "cmd" => "create",
-                            "param" => array(
-                                "table" => "liam3_LoginAttempts",
-                                "row" => array(
-                                    "liam3_LoginAttempts_time" => date('Y-m-d H:i'),
-                                    "liam3_LoginAttempts_info" => $login_attempt_info
-                                )
-                            )
-                        )
-                    ));
-                }
-                if (!$error && ($user[0]['state_id'] != USER_STATE_COMPLETE)) {
-                    $error = 'The state of this user is not complete';
-                    $login_attempt_info = 'Not Successful - ' . $email_input . ' - ' . $error;
-                    $result = api(json_encode(array(
-                            "cmd" => "create",
-                            "param" => array(
-                                "table" => "liam3_LoginAttempts",
-                                "row" => array(
-                                    "liam3_LoginAttempts_time" => date('Y-m-d H:i'),
-                                    "liam3_LoginAttempts_info" => $login_attempt_info
-                                )
-                            )
-                        )
-                    ));
-                }
-            }
-            if (!$error) {
-                $salt = $user[0]['liam3_User_salt'];
-                $hashedPassword = hash('sha512', $password_input . $salt);
-                if ($hashedPassword != $user[0]['liam3_User_password']) {
-                    $error = 'Wrong password.';
-                    $login_attempt_info = 'Not Successful - ' . $email_input . ' - ' . $error;
-                    $result = api(json_encode(array(
-                            "cmd" => "create",
-                            "param" => array(
-                                "table" => "liam3_LoginAttempts",
-                                "row" => array(
-                                    "liam3_LoginAttempts_time" => date('Y-m-d H:i'),
-                                    "liam3_LoginAttempts_info" => $login_attempt_info
-                                )
-                            )
-                        )
-                    ));
-                } else {
-                    //$_SESSION['user_id'] = $user_id;
-                    $login_attempt_info = 'Successful - ' . $email_input;
-                    $result = api(json_encode(array(
-                            "cmd" => "create",
-                            "param" => array(
-                                "table" => "liam3_LoginAttempts",
-                                "row" => array(
-                                    "liam3_LoginAttempts_time" => date('Y-m-d H:i'),
-                                    "liam3_LoginAttempts_info" => $login_attempt_info
-                                )
-                            )
-                        )
-                    ));
+            } else {
+                $error = false;
 
+                $login = json_decode(api(json_encode(array("cmd" => "login", "param" => array("email" => $email_input, "password" => $password_input)))), true);
+                if (isset($login['login_valid']) && $login['login_valid']) {
+                    $token = $login['token'];
                     if (isset($_GET['origin'])) {
                         $origin = $_GET['origin'];
-                        $jwt_token = array(
-                            "iss" => "liam3",
-                            "uid" => $user_id,
-                            "iat" => time(),
-                            "exp" => time() + 86400
-                        );
-                        $token = JWT::encode($jwt_token, AUTH_KEY);
                         header("Location: " . $origin . "?token=" . $token);
                         exit();
                     } else {
-                        $_SESSION['user_id'] = $user_id;
+                        $_SESSION['token'] = $token;
+                    }
+                } else {
+                    $error = $login['error']['msg'];
+                }
+                //var_dump($login);exit();
+                
+
+                /*$email = api(json_encode(array("cmd" => "read", "param" => array("table" => "liam3_email",
+                    "filter" => '{"=":["liam3_email_text","'.$email_input.'"]}'))));
+                    $email = $email['records'];
+                if (!$email) {
+                    $error = 'E-mail address is not recognized by the system, please enter the correct e-mail address or register.';
+                    $login_attempt_info = 'Not Successful - ' . $email_input . ' - ' . $error;
+                    $result = api(json_encode(array(
+                            "cmd" => "create",
+                            "paramparam" => array(
+                                "table" => "liam3_LoginAttempts",
+                                "row" => array(
+                                    "liam3_LoginAttempts_time" => date('Y-m-d H:i'),
+                                    "liam3_LoginAttempts_info" => $login_attempt_info
+                                )
+                            )
+                        )
+                    ));
+                } elseif ($email[0]['state_id'] == EMAIL_STATE_VERIFIED) {
+                    $email_id = $email[0]['liam3_email_id'];
+                } else {
+                    $error = 'Email is not verified';
+                    $login_attempt_info = 'Not Successful - ' . $email_input . ' - ' . $error;
+                    $result = api(json_encode(array(
+                            "cmd" => "create",
+                            "param" => array(
+                                "table" => "liam3_LoginAttempts",
+                                "row" => array(
+                                    "liam3_LoginAttempts_time" => date('Y-m-d H:i'),
+                                    "liam3_LoginAttempts_info" => $login_attempt_info
+                                )
+                            )
+                        )
+                    ));
+                }
+                if (!$error) {
+                    $user_email = json_decode(api(json_encode(array("cmd" => "read", "param" => array("table" => "liam3_user_email",
+                        "filter" => '{"=":["liam3_email_id",'.$email_id.']}')))), true);
+                        $user_email = $user_email['records'];
+                    if ($user_email && ($user_email[0]['state_id'] == USER_EMAIL_STATE_USE)) {
+                        $user_id = $user_email[0]['liam3_User_id_fk_164887']['liam3_User_id'];
+                    } else {
+                        $error = 'This email is unselected.';
+                        $login_attempt_info = 'Not Successful - ' . $email_input . ' - ' . $error;
+                        $result = api(json_encode(array(
+                                "cmd" => "create",
+                                "param" => array(
+                                    "table" => "liam3_LoginAttempts",
+                                    "row" => array(
+                                        "liam3_LoginAttempts_time" => date('Y-m-d H:i'),
+                                        "liam3_LoginAttempts_info" => $login_attempt_info
+                                    )
+                                )
+                            )
+                        ));
                     }
                 }
+                if (!$error) {
+                    $user = json_decode(api(json_encode(array("cmd" => "read", "param" => array("table" => "liam3_user",
+                        "filter" => '{"=":["liam3_User_id",'.$user_id.']}')))), true);
+                    $user = $user['records'];
+                    if (!$user) {
+                        $error = 'This email is not linked to any user.';
+                        $login_attempt_info = 'Not Successful - ' . $email_input . ' - ' . $error;
+                        $result = api(json_encode(array(
+                                "cmd" => "create",
+                                "param" => array(
+                                    "table" => "liam3_LoginAttempts",
+                                    "row" => array(
+                                        "liam3_LoginAttempts_time" => date('Y-m-d H:i'),
+                                        "liam3_LoginAttempts_info" => $login_attempt_info
+                                    )
+                                )
+                            )
+                        ));
+                    }
+                    if (!$error && ($user[0]['state_id'] != USER_STATE_COMPLETE)) {
+                        $error = 'The state of this user is not complete';
+                        $login_attempt_info = 'Not Successful - ' . $email_input . ' - ' . $error;
+                        $result = api(json_encode(array(
+                                "cmd" => "create",
+                                "param" => array(
+                                    "table" => "liam3_LoginAttempts",
+                                    "row" => array(
+                                        "liam3_LoginAttempts_time" => date('Y-m-d H:i'),
+                                        "liam3_LoginAttempts_info" => $login_attempt_info
+                                    )
+                                )
+                            )
+                        ));
+                    }
+                }
+                if (!$error) {
+                    $salt = $user[0]['liam3_User_salt'];
+                    $hashedPassword = hash('sha512', $password_input . $salt);
+                    if ($hashedPassword != $user[0]['liam3_User_password']) {
+                        $error = 'Wrong password.';
+                        $login_attempt_info = 'Not Successful - ' . $email_input . ' - ' . $error;
+                        $result = api(json_encode(array(
+                                "cmd" => "create",
+                                "param" => array(
+                                    "table" => "liam3_LoginAttempts",
+                                    "row" => array(
+                                        "liam3_LoginAttempts_time" => date('Y-m-d H:i'),
+                                        "liam3_LoginAttempts_info" => $login_attempt_info
+                                    )
+                                )
+                            )
+                        ));
+                    } else {
+                        //$_SESSION['user_id'] = $user_id;
+                        $login_attempt_info = 'Successful - ' . $email_input;
+                        $result = api(json_encode(array(
+                                "cmd" => "create",
+                                "param" => array(
+                                    "table" => "liam3_LoginAttempts",
+                                    "row" => array(
+                                        "liam3_LoginAttempts_time" => date('Y-m-d H:i'),
+                                        "liam3_LoginAttempts_info" => $login_attempt_info
+                                    )
+                                )
+                            )
+                        ));
+
+                        if (isset($_GET['origin'])) {
+                            $origin = $_GET['origin'];
+                            $jwt_token = array(
+                                "iss" => "liam3",
+                                "uid" => $user_id,
+                                "iat" => time(),
+                                "exp" => time() + 86400
+                            );
+                            $token = JWT::encode($jwt_token, AUTH_KEY);
+                            header("Location: " . $origin . "?token=" . $token);
+                            exit();
+                        } else {
+                            $_SESSION['user_id'] = $user_id;
+                        }
+                    }
+                }*/
             }
         }
     }
 }
-if (isset($_SESSION['user_id'])) {
+if (isset($_SESSION['token'])) {
     header("Location: index.php");
     exit();
 } else {
--- index.php
+++ index.php
@@ -1,17 +1,43 @@
 <?php
 require_once(__DIR__ . '/inc/LIAM2_Client_header_session.inc.php');
-if (!isset($_SESSION['user_id'])) {
-    header("Location: LIAM2_Client_login.php");
-    exit();
+require_once(__DIR__ . '/inc/php-jwt-master/src/JWT.inc.php');
+use \Firebase\JWT\JWT;
+if (!isset($_SESSION['token'])) {
+    if (!isset($_GET['origin'])) {
+        header("Location: LIAM2_Client_login.php");
+        exit();
+    } else {
+        header("Location: LIAM2_Client_login.php?origin=" . $_GET['origin']);
+        exit();
+    }
 } else {
+    /*
     $user = json_decode(api(json_encode(array(
         "cmd" => "read",
-        "paramJS" => array(
-            "table" => "liam2_User",
-            "filter" => '{"=":["liam2_User_id", '.$_SESSION['user_id'].']}'
+        "param" => array(
+            "table" => "liam3_user",
+            "filter" => '{"=":["liam3_User_id", '.$_SESSION['token'].']}'
         ))
     )), true);
-    $username = $user[0]['liam2_User_firstname'] . ' ' . $user[0]['liam2_User_lastname'];
+    */
+    //$user = JWT::decode()
+
+    $jwt = $_SESSION['token'];
+    $tks = explode('.', $jwt);
+    list($headb64, $bodyb64, $cryptob64) = $tks;
+    $payload = JWT::jsonDecode(JWT::urlsafeB64Decode($bodyb64));
+    $token = $_SESSION['token'];
+    $user = json_decode(api(json_encode(array(
+        "cmd" => "read",
+        "param" => array(
+            "table" => "liam3_user",
+            "filter" => '{"=":["liam3_User_id", '.$payload->uid.']}'
+        ))
+    )), true);
+
+
+    $username = $user["records"][0]['liam3_User_firstname'] . ' ' . $user["records"][0]['liam3_User_lastname'];
+   
     require_once(__DIR__ . '/inc/LIAM2_Client_header.inc.php');
     require_once(__DIR__ . '/inc/templates/LIAM2_Client_main.inc.php');
 }
