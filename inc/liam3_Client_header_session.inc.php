2c004783e53dd5b7016ba6098c7ab5439532c78b
rootmessages BPMspace <5494549+BPMspace@users.noreply.github.com>
2020-01-11 12:08:08 +0100

    Update liam3_Client_header_session.inc.php

--- inc/liam3_Client_header_session.inc.php
+++ inc/liam3_Client_header_session.inc.php
@@ -1,8 +1,8 @@
 <script type="text/javascript" src="js/LIAM3_Client_main.js"></script>
 <?php
 //session_start();
-require_once(__DIR__ . '/LIAM3_Client_api.secret.inc.php');
-require_once(__DIR__ . '/LIAM3_Client_api.inc.php');
+require_once(__DIR__ . '/liam3_Client_api.secret.inc.php');
+require_once(__DIR__ . '/liam3_Client_api.inc.php');
 define('EMAIL_STATE_NOT_VERIFIED', 1);
 define('EMAIL_STATE_VERIFIED', 2);
 define('EMAIL_STATE_DELETED', 4);
