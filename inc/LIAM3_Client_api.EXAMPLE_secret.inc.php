bd34e72a95e6e607aff24a546c1b9d9acde140d1
rootmessages BPMspace <5494549+BPMspace@users.noreply.github.com>
2020-01-11 11:54:54 +0100

    Rename LIAM3_Client_api.EXAMPLE_secret.inc.php to liam3_Client_api.EXAMPLE_secret.inc.php

--- inc/LIAM3_Client_api.EXAMPLE_secret.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-$url="https://BLABLA.at/liam3/";
-$token = "";
-define('LIAM3_URL', 'https://BLABLA.at/liam3_client/'); 
---
+++ inc/liam3_Client_api.EXAMPLE_secret.inc.php
@@ -0,0 +1,4 @@
+<?php
+$url="https://BLABLA.at/liam3/";
+$token = "";
+define('LIAM3_URL', 'https://BLABLA.at/liam3_client/'); 
