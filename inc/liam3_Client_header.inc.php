0bc14f87b02ff801b7fa3e6e0760c4278faeb25a
Boban Ilievski <bilievski@ssidecisions.com>
2020-02-13 13:20:50 +0100

    Merging custom changes.

--- inc/liam3_Client_header.inc.php
+++ inc/liam3_Client_header.inc.php
@@ -4,7 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Title -->
-    <title>LIAM3 Client</title>
+    <?php
+    $current_file = explode('/', $_SERVER['SCRIPT_NAME']);
+    $current_file = end($current_file);
+    $title = str_replace('liam3_Client_', '', $current_file);
+    $title = strstr($title, '.php', true);
+    if ($title == 'index') $title = 'Main';
+    $title = ucfirst(str_replace('_', ' ', $title));
+    ?>
+    <title><?php echo $title; ?></title>
     <!-- CSS via CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
